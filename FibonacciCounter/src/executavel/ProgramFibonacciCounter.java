cb70de468e5b4cc26a647e9cd4486b6bf6cf200b
Luis Henrique Alcantara <luisricksouza@gmail.com>
2024-04-26 20:44:12 -0300

    Feat: Second Program
    Create a program that prints the starting number of the Fibonacci Sequence

---
+++ FibonacciCounter/src/executavel/ProgramFibonacciCounter.java
@@ -0,0 +1,40 @@
+package executavel;
+
+import java.util.Scanner;
+
+public class ProgramFibonacciCounter {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int terms, f1 = 1, f2 = 1, fn = 1;
+
+		System.out.println("This program prints the starting numbers of the Fibonacci Sequence, chosen by you!\n");
+		
+		System.out.print("Number of terms: ");
+		terms = sc.nextInt();
+		System.out.println();
+		
+		for (int i = terms; i > 0; i--) {
+			if (i == terms) {
+				System.out.print(fn+" ");
+			}
+			
+			else if (i == terms - 1) {
+				System.out.print(fn+" ");		
+			}
+			
+			else {
+				fn = f1 + f2;
+				System.out.print(fn+" ");
+				f1 = f2;
+				f2 = fn;
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
---
+++ FibonacciCounter/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module FibonacciCounter {
+}
