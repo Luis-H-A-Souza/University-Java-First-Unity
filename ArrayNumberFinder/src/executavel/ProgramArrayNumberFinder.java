0205e15010e68b91e33bc3eff3d4f2382be03a9d
Luis Souza <luisricksouza@gmail.com>
2024-05-18 10:21:00 -0300

    Fix: Correct sintax

--- ArrayNumberFinder/src/executavel/ProgramArrayNumberFinder.java
+++ ArrayNumberFinder/src/executavel/ProgramArrayNumberFinder.java
@@ -3,6 +3,8 @@ package executavel;
 import java.util.Scanner;
 
 public class ProgramArrayNumberFinder {
+	//A program that reads a 10 position array and a 'N' value. Then search if there's a 'N' value in the array, if so, prints the position
+	//else, prints a message
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -11,7 +13,8 @@ public class ProgramArrayNumberFinder {
 
 		System.out.println("Welcome to Kiko's Number Finder!");
 		System.out.println("Start by filling the array!\n");
-
+		
+		//For fills the array
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(aux + "º Number: ");
 			array[i] = sc.nextInt();
@@ -19,15 +22,20 @@ public class ProgramArrayNumberFinder {
 		}
 		System.out.print("\nInform the Number you want to find: ");
 		int n = sc.nextInt();
-
+		
+		 
+		System.out.println();
+		//For searches the occurrences of the N number
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] == n) {
-				System.out.println("\nNumber encountered in position " + i);
+				System.out.println("Number encountered in position " + i);
+				//'match' variable to confirm an encounter 
 				match++;
 			}
 		}
+		//Depending on the value of 'match' informs the following message 
 		if (match == 0) {
-			System.out.println("\nThere are zero occurrences on that Number!");
+			System.out.println("There are zero occurrences on that Number!");
 		}
 
 	}
