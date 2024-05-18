4d878d29e4a865eae3b10fc1763b34a906d1ed67
Luis Souza <luisricksouza@gmail.com>
2024-05-18 09:58:06 -0300

    Fix: Now it shows the correct values

--- ArrayHigherSmallerNumber/src/executavel/ProgramArrayHigherSmallerNumber.java
+++ ArrayHigherSmallerNumber/src/executavel/ProgramArrayHigherSmallerNumber.java
@@ -3,6 +3,7 @@ package executavel;
 import java.util.Scanner;
 
 public class ProgramArrayHigherSmallerNumber {
+	//Read a 12 position array and print the highest and smallest numbers as so it's respective positions
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -11,14 +12,17 @@ public class ProgramArrayHigherSmallerNumber {
 		System.out.println("Welcome to Kiko's HigherSmallerNumber!");
 		System.out.println("Let's find out the Highest and Smallest Number and they position between 12 Numbers!");
 		System.out.println("\n1º Number: ");
+		//Reads the first number for initializing other variables
 		number[0] = sc.nextInt();
-
+		
+		//Sets some variables based on the first reading for comparison reasons
 		int higherNumber = number[0], smallerNumber = number[0];
 		int higherPos = 0, smallerPos = 0, aux = 2;
 
 		for (int i = 1; i < number.length; i++) {
 			System.out.println(aux + "º Number: ");
 			number[i] = sc.nextInt();
+			//Compares the actual number with previous set values
 			if (number[i] > higherNumber) {
 				higherNumber = number[i];
 				higherPos = i;
