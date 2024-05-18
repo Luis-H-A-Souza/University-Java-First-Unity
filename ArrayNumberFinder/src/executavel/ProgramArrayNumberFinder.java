e5c29f630663383f422dd63c7df66f646390c552
Luis Souza <luisricksouza@gmail.com>
2024-05-18 09:57:29 -0300

    Feat: Create a program that finds a number in an array

--- ArrayNumberFinder/src/executavel/ProgramArrayNumberFinder.java
+++ ArrayNumberFinder/src/executavel/ProgramArrayNumberFinder.java
@@ -1,9 +1,34 @@
 package executavel;
 
+import java.util.Scanner;
+
 public class ProgramArrayNumberFinder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int[] array = new int[10];
+		int aux = 1, match = 0;
+
+		System.out.println("Welcome to Kiko's Number Finder!");
+		System.out.println("Start by filling the array!\n");
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(aux + "º Number: ");
+			array[i] = sc.nextInt();
+			aux++;
+		}
+		System.out.print("\nInform the Number you want to find: ");
+		int n = sc.nextInt();
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == n) {
+				System.out.println("\nNumber encountered in position " + i);
+				match++;
+			}
+		}
+		if (match == 0) {
+			System.out.println("\nThere are zero occurrences on that Number!");
+		}
 
 	}
 
